6bcadad806e17fecfd63f025a2d71edf116e2ebd
Laxman Sooriyathas <laxman0202@gmail.com>
2022-04-25 12:32:22 -0400

    Use node environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'public',
-			assets: 'public'
-		})
+		adapter: adapter()
 }
 };
 
