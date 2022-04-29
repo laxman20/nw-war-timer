124f01c09176f01dd19fc1e57d26fe85182b5190
Laxman Sooriyathas <laxman0202@gmail.com>
2022-04-29 19:27:01 -0400

    remove query param from url when clearing input

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-  import Timer from '$lib/components/Timer.svelte';
+	import Timer from '$lib/components/Timer.svelte';
 
 	let intervals = $page.url.searchParams.get('intervals');
 
 	const textChanged = function () {
+		const url = new URL(window.location.origin);
 		if (intervals) {
-			const url = new URL(window.location.toString());
 			url.searchParams.set('intervals', intervals);
-			history.replaceState({}, '', url.href);
+		} else {
+			url.searchParams.delete('intervals');
 		}
+		history.replaceState({}, '', url.href);
 	};
 </script>
 
 <label for="intervals" class="font-medium text-gray-900">Intervals</label>
 <textarea
 	name="intervals"
-  placeholder="10,10"
+	placeholder="10,10"
 	class="
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-dark
-        bg-white bg-clip-padding
-        border border-solid border-gray
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-gray-600 focus:outline-none
-      "
+	block
+	w-full
+	px-3
+	py-1.5
+	text-base
+	font-normal
+	text-gray-dark
+	bg-white bg-clip-padding
+	border border-solid border-gray
+	rounded
+	transition
+	ease-in-out
+	m-0
+	focus:text-gray-700 focus:bg-white focus:border-gray-600 focus:outline-none
+	"
 	bind:value={intervals}
 	on:input={textChanged}
 />
